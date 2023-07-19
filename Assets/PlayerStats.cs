e2cba7f5e3a319ed894cf186868f00a45f61f18b
omerakcali <omerakcali99@gmail.com>
2023-07-20 01:22:05 +0300

    player stats scriptable obj

---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Objects/Player Stats")]
+public class PlayerStats : ScriptableObject
+{
+    public float MoveSpeed;
+}
