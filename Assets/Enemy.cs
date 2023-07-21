90a9d60e801820194d6fe3fb22dc25347b532d50
omerakcali <omerakcali99@gmail.com>
2023-07-22 00:00:34 +0300

    enemy pool

---
+++ Assets/Enemy.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float Hp { get; private set; }
+
+    public void ResetToDefault()
+    {
+        
+    }
+}
---
+++ Assets/EnemyPool.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPool : Service<EnemyPool>
+{
+    [SerializeField] 
+    private Enemy EnemyPrefab;
+
+    [SerializeField] 
+    private Transform PoolRoot;
+    
+    [SerializeField] 
+    private int PoolInitSize = 20;
+
+    private List<Enemy> _reservedItems = new();
+    
+    internal override void Init()
+    {
+        PopulatePool();
+    }
+
+    private void PopulatePool()
+    {
+        for (int i = 0; i < PoolInitSize; i++)
+        {
+            var instance = Instantiate(EnemyPrefab,PoolRoot);
+            instance.ResetToDefault();
+            instance.gameObject.SetActive(false);
+            _reservedItems.Add(instance);
+        }
+    }
+
+    public Enemy Request()
+    {
+        if (_reservedItems.Count > 0)
+        {
+            var instance = _reservedItems[^1];
+            _reservedItems.RemoveAt(_reservedItems.Count-1);
+            instance.ResetToDefault();
+            return instance;
+        }
+        else
+        {
+            var instance = Instantiate(EnemyPrefab);
+            instance.ResetToDefault();
+            return instance;
+        }
+    }
+
+    public void Return(Enemy enemy)
+    {
+        _reservedItems.Add(enemy);
+        enemy.gameObject.SetActive(false);
+        enemy.transform.SetParent(PoolRoot);
+    }
+}
